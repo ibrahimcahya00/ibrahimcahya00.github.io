07249b950460ce8a2ac8387b1a4e097462f58067
ibrahimcahya00 <116746441+ibrahimcahya00@users.noreply.github.com>
2022-10-26 23:07:55 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#0EC5D7',
+                secondary: '#E18700 100%, #FF32AD 100%, #0EC5D7 100%',
+            },
+            fontFamily: {
+                base: ['Nunito', 'sans-serif'],
+                nav: ['Raleway', 'sans-serif']
+            },
+            fontSize: {
+                'heading-1': '50px',
+            },
+        },
+    }
+}
